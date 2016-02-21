3a2bc310f03d290b6663d78845d11f5bcf8a89e2
hariaakash <smgdark@gmail.com>
2016-02-21 04:55:00 -0800

    Update footer.php

--- blocks/footer.php
+++ blocks/footer.php
@@ -8,7 +8,6 @@
             </ul>
             <p>Made by <a href="http://hariaakash.tk">Hari Aakash</a>. Contact him at <a href="mailto:smgdark@gmail.com">smgdark@gmail.com</a>.</p>
             <p>Hosting by <a href="http://trekgameservers.com">Trek Game Servers</a>.</p>
-            <p>Artwork by <a href="http://axelblazer88.tk">Axel Blazer</a>.</p>
             <p>Code licensed under the <a href="http://www.apache.org/licenses/LICENSE-2.0">Apache License v2.0</a>.</p>
             <p>Based on <a href="http://getbootstrap.com">Bootstrap</a>. Icons from <a href="http://fortawesome.github.io/Font-Awesome/">Font Awesome</a>. Web fonts from <a href="http://www.google.com/webfonts">Google</a>.</p>
 
