bdecd252ce938931c3ebe10d7be9c0fce6cfb854
hariaakash <smgdark@gmail.com>
2016-02-21 03:57:24 -0800

    Added all files
    Blocks, Js, Css and Index

---
+++ blocks/panels-normal.php
@@ -0,0 +1,19 @@
+<div class="col-lg-4">        
+            <div class="panel panel-default">
+              <div class="panel-body">
+                Basic panel
+              </div>
+            </div>
+            <div class="panel panel-default">
+              <div class="panel-heading">Panel heading</div>
+              <div class="panel-body">
+                Panel content
+              </div>
+            </div>
+            <div class="panel panel-default">
+              <div class="panel-body">
+                Panel content
+              </div>
+              <div class="panel-footer">Panel footer</div>
+            </div>
+          </div>
---
+++ blocks/wells.php
@@ -0,0 +1,17 @@
+<div class="row">
+          <div class="col-lg-4">
+            <div class="well">
+              Look, I'm in a well!
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="well well-sm">
+              Look, I'm in a small well!
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="well well-lg">
+              Look, I'm in a large well!
+            </div>
+          </div>
+        </div>
