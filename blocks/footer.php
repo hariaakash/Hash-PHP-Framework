ce28ebe1e36976e69547d17a022840a7e9b16dbf
hariaakash <smgdark@gmail.com>
2016-02-21 04:51:32 -0800

    Update footer.php

--- blocks/footer.php
+++ blocks/footer.php
@@ -4,18 +4,13 @@
             
             <ul class="list-unstyled">
               <li class="pull-right"><a href="#top">Back to top</a></li>  
-              
-              <li>
-<img src="https://www.cloudflare.com/media/images/web-badges/cf-web-badges-g-blue.png">
-</li>
+         
             </ul>
-            <p><span class="label label-danger">CSAT Secure</span> © 2006-2014</p>
-            <p>Made by <a href="http://vortex1409.tk">Michael Dorfman</a>. Contact him at <a href="mailto:vortex14@frostbitetech.org">vortex14@frostbitetech.org</a>.</p>
-            <p>FRAMEWORK by <a href="http://vortex1409.tk">Michael Dorfman</a>.</p>
-            <p>Hosting by <a href="http://vortex1409.tk">Frostbite Technology, LLC</a>.</p>
+            <p>Made by <a href="http://hariaakash.tk">Hari Aakash</a>. Contact him at <a href="mailto:smgdark@gmail.com">smgdark@gmail.com</a>.</p>
+            <p>Hosting by <a href="http://trekgameservers.com">Trek Game Servers</a>.</p>
             <p>Artwork by <a href="http://axelblazer88.tk">Axel Blazer</a>.</p>
             <p>Code licensed under the <a href="http://www.apache.org/licenses/LICENSE-2.0">Apache License v2.0</a>.</p>
-            <p>Based on <a href="http://getbootstrap.com">Bootstrap</a>. Icons from <a href="http://fortawesome.github.io/Font-Awesome/">Font Awesome</a>. Web fonts from <a href="http://www.google.com/webfonts">Google</a>. Favicon by <a href="http://vortex1409.tk">Michael "VORTEX1409" Dorfman</a>.</p>
+            <p>Based on <a href="http://getbootstrap.com">Bootstrap</a>. Icons from <a href="http://fortawesome.github.io/Font-Awesome/">Font Awesome</a>. Web fonts from <a href="http://www.google.com/webfonts">Google</a>.</p>
 
           </div>
         </div>
